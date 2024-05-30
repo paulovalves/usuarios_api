baa85c6e7060fda7ae4d5a0973bd0e3664e8bfa8
paulovalves <paulovalves@outlook.com>
2024-05-30 14:41:54 -0300

    chore: initial commit

---
+++ src/main/java/com/teste/vm_tecnologia/VmTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.teste.vm_tecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmTecnologiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmTecnologiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teste/vm_tecnologia/VmTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.vm_tecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmTecnologiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
