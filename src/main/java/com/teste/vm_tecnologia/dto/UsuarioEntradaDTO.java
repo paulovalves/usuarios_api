df012341e3116a735ebeb9acc57ae1a7a7935936
paulovalves <paulovalves@outlook.com>
2024-05-31 11:24:05 -0300

    refactor: add input and output DTOs for Usuario

---
+++ src/main/java/com/teste/vm_tecnologia/dto/UsuarioEntradaDTO.java
@@ -0,0 +1,12 @@
+package com.teste.vm_tecnologia.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioEntradaDTO {
+    private String nome;
+    private String email;
+    private String senha;
+}
---
+++ src/main/java/com/teste/vm_tecnologia/dto/UsuarioSaidaDTO.java
@@ -0,0 +1,12 @@
+package com.teste.vm_tecnologia.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioSaidaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+}
