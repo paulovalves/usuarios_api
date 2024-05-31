f25424f14eac3881f7d677b37f3d001576dbbf77
paulovalves <paulovalves@outlook.com>
2024-05-31 06:13:27 -0300

    feat: add exception handler for when the user already exists in the database

---
+++ src/main/java/com/teste/vm_tecnologia/model/exceptions/UsuarioExisteException.java
@@ -0,0 +1,7 @@
+package com.teste.vm_tecnologia.model.exceptions;
+
+public class UsuarioExisteException extends Exception{
+    public UsuarioExisteException(String message) {
+        super(message);
+    }
+}
