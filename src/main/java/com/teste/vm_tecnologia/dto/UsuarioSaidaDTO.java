95833230404f03a973549cef995b101331b2816c
paulovalves <paulovalves@outlook.com>
2024-06-01 05:46:33 -0300

    refactor: simplify the UsuarioSaidaDTO map

--- src/main/java/com/teste/vm_tecnologia/dto/UsuarioSaidaDTO.java
+++ src/main/java/com/teste/vm_tecnologia/dto/UsuarioSaidaDTO.java
@@ -1,12 +1,30 @@
 package com.teste.vm_tecnologia.dto;
 
+import com.teste.vm_tecnologia.model.Usuario;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 @Getter
 @Setter
-public class UsuarioSaidaDTO {
+@NoArgsConstructor
+public class UsuarioSaidaDTO extends RepresentationModel<UsuarioSaidaDTO> {
     private Long id;
     private String nome;
     private String email;
+
+    public UsuarioSaidaDTO(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.email = usuario.getEmail();
+    }
+
+    public UsuarioSaidaDTO(Optional<Usuario> usuario) {
+        this.id = usuario.map(Usuario::getId).orElse(null);
+        this.nome = usuario.map(Usuario::getNome).orElse(null);
+        this.email = usuario.map(Usuario::getEmail).orElse(null);
+    }
 }
