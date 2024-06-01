4ef742e4cf1393bf4bea479fe1da1cc02ee31a09
paulovalves <paulovalves@outlook.com>
2024-06-01 05:46:42 -0300

    feat: add findAll pageable service

--- src/main/java/com/teste/vm_tecnologia/service/UsuarioService.java
+++ src/main/java/com/teste/vm_tecnologia/service/UsuarioService.java
@@ -9,6 +9,9 @@ import com.teste.vm_tecnologia.model.exceptions.UsuarioJaExisteException;
 import com.teste.vm_tecnologia.model.exceptions.UsuarioNaoExisteException;
 import com.teste.vm_tecnologia.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,13 +48,24 @@ public class UsuarioService {
             if (usuario.isEmpty() || usuario.get().getEmail() == null || usuario.get().getEmail().isEmpty()) {
                 throw new UsuarioNaoExisteException(MessageEnum.USUARIO_NAO_ENCONTRADO.getMessage());
             }
-            UsuarioSaidaDTO usuarioSaidaDTO = mapToSaida(usuario);
+            UsuarioSaidaDTO usuarioSaidaDTO = new UsuarioSaidaDTO(usuario);
             return new APIResponse<>(MessageEnum.SUCESSO_BUSCAR_USUARIO.getMessage(), usuarioSaidaDTO);
         } catch (UsuarioNaoExisteException e) {
             throw new UsuarioNaoExisteException(e.getMessage());
         }
     }
 
+    // findAll page
+    public Page<UsuarioSaidaDTO> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        try {
+            Page<Usuario> usuarios = usuarioRepository.findAll(pageable);
+            return usuarios.map(UsuarioSaidaDTO::new);
+        } catch (Exception e) {
+            System.err.println(MessageEnum.ERRO_BUSCAR_USUARIOS + e.getMessage());
+            throw new RuntimeException(MessageEnum.ERRO_BUSCAR_USUARIOS.getMessage());
+        }
+    }
     private Usuario mapToUsuario(UsuarioEntradaDTO usuarioEntradaDTO) {
         Usuario usuario = new Usuario();
         usuario.setNome(usuarioEntradaDTO.getNome());
