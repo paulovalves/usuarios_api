b2198921e80f959d16dc2d54ed85964e57f319fb
paulovalves <paulovalves@outlook.com>
2024-06-01 10:33:48 -0300

    feat: add annotations for constructors

--- src/main/java/com/teste/vm_tecnologia/dto/UsuarioEntradaDTO.java
+++ src/main/java/com/teste/vm_tecnologia/dto/UsuarioEntradaDTO.java
@@ -1,13 +1,17 @@
 package com.teste.vm_tecnologia.dto;
 
 import com.teste.vm_tecnologia.model.Usuario;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Optional;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioEntradaDTO {
     private String nome;
     private String email;
