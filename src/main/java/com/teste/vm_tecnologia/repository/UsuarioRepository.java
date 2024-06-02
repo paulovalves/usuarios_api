6d6b2103d2cc29de803af787518e9ff6a4040fe7
paulovalves <paulovalves@outlook.com>
2024-06-02 08:03:57 -0300

    fix: add Optional return to findById repository method

--- src/main/java/com/teste/vm_tecnologia/repository/UsuarioRepository.java
+++ src/main/java/com/teste/vm_tecnologia/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import com.teste.vm_tecnologia.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
