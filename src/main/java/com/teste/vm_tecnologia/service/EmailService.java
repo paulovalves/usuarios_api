c063e399b12a83ff089105bc3e351b855b614f88
paulovalves <paulovalves@outlook.com>
2024-06-02 17:29:11 -0300

    feat: add email service and configurations

---
+++ src/main/java/com/teste/vm_tecnologia/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.teste.vm_tecnologia.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String to, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        System.out.println("Email enviado para: " + to);
+    }
+}
