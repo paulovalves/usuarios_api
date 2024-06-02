a2f2f9cb7b3ab749cc89ebd1c50741b69c364d6e
paulovalves <paulovalves@outlook.com>
2024-06-02 08:02:27 -0300

    refactor: add user login based security

--- src/main/java/com/teste/vm_tecnologia/config/security/SecurityConfig.java
+++ src/main/java/com/teste/vm_tecnologia/config/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.teste.vm_tecnologia.config.security;
 
+import com.teste.vm_tecnologia.model.enums.MessageEnum;
+import com.teste.vm_tecnologia.model.exceptions.UsuarioNaoExisteException;
+import com.teste.vm_tecnologia.repository.UsuarioRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,15 +16,22 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Optional;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig implements UserDetailsService{
+
+    private final UsuarioRepository usuarioRepository;
+
+    public SecurityConfig(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -28,23 +39,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests(authorizeRequests -> authorizeRequests
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(HttpMethod.GET, "/api/usuario/buscar", "/api/usuario/buscar/{id}").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/usuario/salvar").anonymous()
-                ).httpBasic(httpBasic -> {});
+                        .requestMatchers(HttpMethod.POST, "/api/usuario/salvar").permitAll()
+                ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
@@ -56,5 +57,23 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder)
                 .and()
                 .build();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        try {
+            var usuario = usuarioRepository.findByEmail(email);
+            if(usuario.isPresent() && usuario.get().getEmail() != null) {
+                return User.builder()
+                    .username(usuario.get().getEmail())
+                    .password(usuario.get().getSenha())
+                    .roles("USER")
+                    .build();
+            } else {
+                throw new UsuarioNaoExisteException(MessageEnum.USUARIO_NAO_ENCONTRADO.getMessage());
+            }
+        } catch (UsuarioNaoExisteException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
