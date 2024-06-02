566e2027e58c4754a38335b604d4f6cf4a9f8999
paulovalves <paulovalves@outlook.com>
2024-06-02 08:05:14 -0300

    refactor: add user login based security to findById service method

--- src/main/java/com/teste/vm_tecnologia/service/UsuarioService.java
+++ src/main/java/com/teste/vm_tecnologia/service/UsuarioService.java
@@ -5,23 +5,37 @@ import com.teste.vm_tecnologia.dto.UsuarioSaidaDTO;
 import com.teste.vm_tecnologia.model.APIResponse;
 import com.teste.vm_tecnologia.model.Usuario;
 import com.teste.vm_tecnologia.model.enums.MessageEnum;
+import com.teste.vm_tecnologia.model.exceptions.UnauthorizedException;
 import com.teste.vm_tecnologia.model.exceptions.UsuarioJaExisteException;
 import com.teste.vm_tecnologia.model.exceptions.UsuarioNaoExisteException;
 import com.teste.vm_tecnologia.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private UserDetailsService userDetailsService;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, UserDetailsService userDetailsService) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.userDetailsService = userDetailsService;
+    }
+
 
     @Transactional(rollbackFor = Exception.class)
     public APIResponse<UsuarioSaidaDTO> save(UsuarioEntradaDTO usuarioEntradaDTO) throws UsuarioJaExisteException {
@@ -29,10 +43,10 @@ public class UsuarioService {
         Usuario usuario = Usuario.builder()
                 .nome(usuarioEntradaDTO.getNome())
                 .email(usuarioEntradaDTO.getEmail())
-                .senha(usuarioEntradaDTO.getSenha())
+                .senha(passwordEncoder.encode(usuarioEntradaDTO.getSenha()))
                 .build();
-        Usuario usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
-        if (usuarioExistente != null) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+        if (usuarioExistente.isPresent() && usuarioExistente.get().getEmail() != null) {
             throw new UsuarioJaExisteException(MessageEnum.USUARIO_EXISTE.getMessage());
         }
         try {
@@ -50,16 +64,32 @@ public class UsuarioService {
         }
     }
 
-    public APIResponse<UsuarioSaidaDTO> findById(Long id) throws UsuarioNaoExisteException {
+    public APIResponse<UsuarioSaidaDTO> findById(Long id, String authorizationHeader) throws UsuarioNaoExisteException, UnauthorizedException {
         try {
             Optional<Usuario> usuario = usuarioRepository.findById(id);
             if (usuario.isEmpty() || usuario.get().getEmail() == null || usuario.get().getEmail().isEmpty()) {
                 throw new UsuarioNaoExisteException(MessageEnum.USUARIO_NAO_ENCONTRADO.getMessage());
             }
+
+            checkCreds(authorizationHeader);
+
             UsuarioSaidaDTO usuarioSaidaDTO = UsuarioSaidaDTO.from(usuario.get());
             return new APIResponse<>(MessageEnum.SUCESSO_BUSCAR_USUARIO.getMessage(), usuarioSaidaDTO);
         } catch (UsuarioNaoExisteException e) {
             throw new UsuarioNaoExisteException(e.getMessage());
+        } catch (UnauthorizedException e) {
+            throw new UnauthorizedException(e.getMessage());
+        }
+    }
+
+    private void checkCreds(String authorizationHeader) throws UnauthorizedException {
+        String[] credentials = extractCredentialsFromHeader(authorizationHeader);
+        String email = credentials[0];
+        String senha = credentials[1];
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        if(!passwordEncoder.matches(senha, userDetails.getPassword())) {
+            throw new UnauthorizedException("senha inválido.");
         }
     }
 
@@ -72,5 +102,16 @@ public class UsuarioService {
             System.err.println(MessageEnum.ERRO_BUSCAR_USUARIOS + e.getMessage());
             throw new RuntimeException(MessageEnum.ERRO_BUSCAR_USUARIOS.getMessage());
         }
+    }
+
+    private String[] extractCredentialsFromHeader(String authHeader) throws UnauthorizedException {
+        if(authHeader == null || !authHeader.startsWith("Basic ")) {
+            throw new UnauthorizedException(MessageEnum.USUARIO_NAO_AUTORIZADO.getMessage());
+        }
+
+        String base64Credentials = authHeader.substring(6);
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+        String decodedCreds = new String(decodedBytes, StandardCharsets.UTF_8);
+        return decodedCreds.split(":", 2);
     }
 }
